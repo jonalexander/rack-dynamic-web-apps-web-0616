224eba08f2114c534ee5f315e8b51b9502801506
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-24 11:14:56 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,14 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+ 
+    nums = [
+      num_1 = Kernel.rand(1..20),
+      num_2 = Kernel.rand(1..20),
+      num_3 = Kernel.rand(1..20)
+    ]
+    resp.write "#{num_1} | #{num_2} | #{num_3}\n"
+    nums.uniq.length == 1 ? (resp.write "You Win") : (resp.write "You Lose") 
     resp.finish
   end
 
